636111d436720471d21e24809bcf28431304caf3
HassanMunene <awanzihassan@gmail.com>
2024-03-13 10:24:37 +0300

    technical interview

---
+++ 0x00-python-hello_world/exe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+int main(void)
+{
+    node *head = NULL;
+    node *pointer_to_node;
+    pointer_to_node = add_integer_node(&head, 0);
+    return 0;
+}
