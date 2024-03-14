da661a486997064ad1068c2406b362b8551fd936
HassanMunene <awanzihassan@gmail.com>
2024-03-14 22:01:52 +0300

    adhereing to pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,11 +3,9 @@
 Here we are importing a compiled python file hidden_4.pyc
 we will then investigate this file to print its namespaces
 """
-
 if __name__ == "__main__":
-    import  hidden_4
+    import hidden_4
     namespaces = dir(hidden_4)
-
     for name in namespaces:
         if name[:2] != '__':
             print(name)
