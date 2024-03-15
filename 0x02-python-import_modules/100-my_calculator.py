8624c04ff7a3021c53bd90e74cea044de2b97028
HassanMunene <awanzihassan@gmail.com>
2024-03-15 15:17:46 +0300

    build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    args_len = len(sys.argv)
+    if args_len != 4:
+        print(f"Usage: {sys.argv[0]} <a> <operator> <b>")
+        sys.exit(1)
+
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[3])
+    arg_op = sys.argv[2]
+    add_op = '+'
+    sub_op = '-'
+    mul_op = '*'
+    div_op = '/'
+
+    if arg_op == add_op:
+        print(f'{num1} + {num2} = {num1+num2}')
+    elif arg_op == sub_op:
+        print(f'{num1} - {num2} = {num1-num2}')
+    elif arg_op == mul_op:
+        print(f'{num1} * {num2} = {num1*num2}')
+    elif arg_op == div_op:
+        print(f'{num1} / {num2} = {float(num1/num2)}')
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        sys.exit(1)
