2d9fd871d5eb48542f289d207c01eeb7c58044d1
HassanMunene <awanzihassan@gmail.com>
2024-03-18 11:10:07 +0300

    using the if else loop

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,21 +3,26 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     lenA = len(tuple_a)
     lenB = len(tuple_b)
-    tuple_len = 2
 
-    if lenA < tuple_len or lenB < tuple_len:
-        tuple_a = tuple_a + (0, ) *(tuple_len - lenA)
-        tuple_b = tuple_b + (0, ) *(tuple_len - lenB)
+    #if tuple a is empty the it should be (0, 0)
+    if lenA < 1:
+        tuple_a = 0,0
+    elif lenA < 2:
+        tuple_a = tuple_a[0], 0
+    elif lenA > 2:
+        tuple_a = tuple_a[0], tuple_a[1]
 
-    if lenA >= tuple_len or lenB >= tuple_b:
-        tuple_a = tuple_a[:2]
-        tuple_b = tuple_b[:2]
+    if lenB < 1:
+        tuple_b = 0, 0
+    elif lenB < 2:
+        tuple_b = tuple_b[0], 0
+    elif lenB > 2:
+        tuple_b = tuple_b[0], tuple_b[1]
 
     a, b = tuple_a
     c, d = tuple_b
 
     e = a + c
     f = b + d
-
     new_tuple = e, f
     return new_tuple
