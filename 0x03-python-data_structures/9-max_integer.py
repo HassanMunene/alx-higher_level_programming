7012bb6f53d2a0ce13c837387e3ca9675d7fdccd
HassanMunene <awanzihassan@gmail.com>
2024-03-18 12:36:45 +0300

    changing the range config

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,12 @@
 
 def max_integer(my_list=[]):
     list_len = len(my_list)
+    if list_len == 0:
+        return
     max = my_list[0]
-    for i in range(list_len - 1):
+    for i in range(list_len):
         if max < my_list[i]:
             max = my_list[i]
+        else:
+            continue
     return max
