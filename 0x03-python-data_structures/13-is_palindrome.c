dd043ad46fa2f6b713ef2353a8fcc11f71fdffdf
HassanMunene <awanzihassan@gmail.com>
2024-03-19 21:39:36 +0300

    betty style correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,69 +1,55 @@
 #include "lists.h"
 #include <stdio.h>
 
+listint_t *find_middle_node(listint_t **head);
+listint_t *reverse_secondHalf_list(listint_t *head);
+int compare_first_second_lists(listint_t *head, listint_t *secondHalf_head);
+
 int is_palindrome(listint_t **head)
 {
-    // we are receiving the the pointer of the head linked list
-    
-    //we first have to indee there is a linked list
-    if (*head == NULL)
-    {
-        return 1;
-    }
-    // find the middle node of the linked list so that we can half it.
-    listint_t *find_middle_node()
-    {
-        listint_t *slow = *head;
-        listint_t *fast = *head;
+listint_t *middleNode = find_middle_node(head);
+listint_t *new_secondHalf_head = reverse_secondHalf_list(middleNode->next);
+return (compare_first_second_lists(*head, new_secondHalf_head));
+}
 
-        while (fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-    listint_t *middleNode = find_middle_node();
-    printf("\n");
-    printf("Middle node is: %d\n", middleNode->n);
+listint_t *find_middle_node(listint_t **head)
+{
+listint_t *slow = *head;
+listint_t *fast = *head;
 
-    // after finding the middle node we have to reverse the second half of the linked list
+while (fast && fast->next)
+{
+slow = slow->next;
+fast = fast->next->next;
+}
+return (slow);
+}
 
-    listint_t *reverse_secondHalf_list(listint_t *head)
-    {
-        listint_t *prev = NULL;
-        listint_t *temp;
-        
-        while (head != NULL)
-        {
-            temp = head->next;
-            head->next = prev;
-            prev = head;
-            head = temp;
-        }
-        return prev;
-    }
-    listint_t *new_secondHalf_head = reverse_secondHalf_list(middleNode->next);
-    printf("first num of reversed list: %d\n", new_secondHalf_head->n);
+listint_t *reverse_secondHalf_list(listint_t *head)
+{
+listint_t *prev = NULL;
+listint_t *temp;
 
-    /* after reversing the second list then we iterate through both list
-     * i.e firstHalf and secondHalf list simultaneously while comparing
-     * there values. if values are not equal return 1 else return 0
-     *
-     */
-    int compare_firstHalf_secondHalf_lists(listint_t *head, listint_t *secondHalf_head)
-    {
-        while (head != NULL && secondHalf_head != NULL)
-        {
-            printf("%d %d\n", head->n, secondHalf_head->n);
-            if (head->n != secondHalf_head->n)
-            {
-                return 0;
-            }
-            head = head->next;
-            secondHalf_head = secondHalf_head->next;
-        }
-        return 1;
-    }
-    return compare_firstHalf_secondHalf_lists(*head, new_secondHalf_head);
+while (head != NULL)
+{
+temp = head->next;
+head->next = prev;
+prev = head;
+head = temp;
+}
+return (prev);
+}
+int compare_first_second_lists(listint_t *head, listint_t *secondHalf_head)
+{
+while (head != NULL && secondHalf_head != NULL)
+{
+printf("%d %d\n", head->n, secondHalf_head->n);
+if (head->n != secondHalf_head->n)
+{
+return (0);
+}
+head = head->next;
+secondHalf_head = secondHalf_head->next;
+}
+return (1);
 }
