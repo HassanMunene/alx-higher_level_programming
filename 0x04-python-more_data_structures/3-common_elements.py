efac111b5d23aec1991556aa429374dd7611a7cf
HassanMunene <awanzihassan@gmail.com>
2024-03-20 15:39:09 +0300

    use the union math expression to get common elements in both sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def common_elements(set_1, set_2):
+    """use the union mathematical exp to get elements in both set"""
+    common_set = set_1 & set_2
+    return common_set
