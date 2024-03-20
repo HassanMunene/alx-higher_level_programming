857ce10684ac84048ad5cad9acba6c1e2b9ce1b9
HassanMunene <awanzihassan@gmail.com>
2024-03-20 17:49:51 +0300

    update or add a key-value pair in a dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,5 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    dict_list = list(a_dictionary)
-    for item in dict_list:
-        if item != key:
-            a_dictionary[key] = value
-        elif item == key:
-            a_dictionary[item] = value
+    a_dictionary[key] = value
     return a_dictionary
