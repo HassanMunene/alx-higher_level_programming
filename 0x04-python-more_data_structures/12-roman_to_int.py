d27dadc1205378049481791a4fe341246dd77fc3
HassanMunene <awanzihassan@gmail.com>
2024-03-21 09:45:22 +0300

    converting roman numbers to integers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,49 @@
+#!/user/bin/python3
+"""
+convert a roman num to int
+"""
+
+
+def roman_to_int(roman_string):
+    if isinstance(roman_string, str) is False:
+        return 0
+    if roman_string is None:
+        return 0
+
+    answer = 0
+    """handle edge cases"""
+    if "CM" in roman_string:
+        answer += 900
+        roman_string = roman_string.replace("CM", "")
+    if "CD" in roman_string:
+        answer += 400
+        roman_string = roman_string.replace("CD", "")
+    if "XC" in roman_string:
+        answer += 90
+        roman_string = roman_string.replace("XC", "")
+    if "XL" in roman_string:
+        answer += 40
+        roman_string = roman_string.replace("XL", "")
+    if "IX" in roman_string:
+        answer += 9
+        roman_string = roman_string.replace("IX", "")
+    if "IV" in roman_string:
+        answer += 4
+        roman_string = roman_string.replace("IV", "")
+
+    for i in roman_string:
+        if i == "M":
+            answer += 1000
+        elif i == "D":
+            answer += 500
+        elif i == "C":
+            answer += 100
+        elif i == "L":
+            answer += 50
+        elif i == "X":
+            answer += 10
+        elif i == "V":
+            answer += 5
+        elif i == "I":
+            answer += 1
+    return answer
