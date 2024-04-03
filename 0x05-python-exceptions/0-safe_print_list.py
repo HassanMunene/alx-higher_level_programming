86eccdecf929d0433822603c178dba9ef5eb6124
HassanMunene <awanzihassan@gmail.com>
2024-04-03 11:16:58 +0300

    added a new line print()

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print(f"{my_list[i]}", end="")
             sum += 1
     except IndexError:
+        print()
         pass
     else:
         print()
