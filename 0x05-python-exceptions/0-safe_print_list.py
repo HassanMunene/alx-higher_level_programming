508f86385460ae0038e3073c17e8828956805075
HassanMunene <awanzihassan@gmail.com>
2024-04-03 11:10:15 +0300

    safely printing the values of a list and returning the number of elements in the list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        sum = 0
+        for i in range(x):
+            print(f"{my_list[i]}", end="")
+            sum += 1
+    except IndexError:
+        pass
+    else:
+        print()
+    finally:
+        return sum
