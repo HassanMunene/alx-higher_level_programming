c8782a47dca6e2cc7ecdd21ece249b25c0cdb525
HassanMunene <awanzihassan@gmail.com>
2024-04-04 15:23:34 +0300

    using is not in place of !=

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     class Square with size
     """
     def __init__(self, size=0):
-        if isinstance(size, int) != True:
+        if isinstance(size, int) is not True:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
