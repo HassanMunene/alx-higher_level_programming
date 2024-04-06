8eff9ce857619fea5ffd269fee6fed2f8c4663e2
HassanMunene <awanzihassan@gmail.com>
2024-04-06 19:56:54 +0300

    used to convert lower case to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    new_l = ""
+    for l in str:
+        if 97 <= ord(l) <= 122:
+            new_l += chr(ord(l) - 32)
+        elif 65 <= ord(l) <= 90:
+            new_l += l
+        elif ord(l) == 32 or l.isspace():
+            new_l += " "
+        else:
+            new_l += l
+    print("{}".format(new_l), end='')
+    print()
+
+uppercase("Best School 98 Battery street")
