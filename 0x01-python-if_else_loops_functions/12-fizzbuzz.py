c154bc15a74b46f6c4c4870a42f496d12cad1863
HassanMunene <awanzihassan@gmail.com>
2024-04-06 21:25:05 +0300

    fizzbuzz challenge

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,14 +2,14 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz"
+            print("{} ".format(i), end='')
+        elif i % 3 == 0:
             i = "Fizz"
             print("{} ".format(i), end='')
         elif i % 5 == 0:
             i = "Buzz"
-            print("{} ".format(i), end='')
-        elif i % 5 == 0 and i % 3 == 0:
-            i = "FizzBuzz"
             print("{} ".format(i), end='')
         else:
             print("{} ".format(i), end='')
