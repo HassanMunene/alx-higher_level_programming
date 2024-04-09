4110a3f02830660427c0751478a4cc37ac79fba0
HassanMunene <awanzihassan@gmail.com>
2024-04-09 12:04:41 +0300

    minimize code details

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,14 +9,8 @@ class Square:
     class Square with size
     """
     def __init__(self, size=0, position=(0, 0)):
-        if isinstance(size, int) is not True:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        if isinstance(position, tuple) is False:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
