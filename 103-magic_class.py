6edf835e0ae3326f20b082c499d831465854e772
HassanMunene <awanzihassan@gmail.com>
2024-04-11 19:49:03 +0300

    calculate area and circumference of a circle

---
+++ 103-magic_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    '''class with circle methods'''
+    def __init__(self, radius=0):
+        self.__radius = 0
+
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        '''return area of circle'''
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        '''return circumference of circle'''
+        return 2 * math.pi * self.__radius
