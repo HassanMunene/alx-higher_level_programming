b5e4bf345a67f60a8634cbfe24b970283991130a
HassanMunene <awanzihassan@gmail.com>
2024-04-16 23:46:21 +0300

    using the type function

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -9,7 +9,4 @@ def is_same_class(obj, a_class):
     determine if indeed an obj is an
     instance of the specified class
     """
-    if isinstance(obj, a_class) is True:
-        return True
-    else:
-        return False
+    return type(obj) == a_class
