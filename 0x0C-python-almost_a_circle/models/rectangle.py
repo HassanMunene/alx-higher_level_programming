5819bc6af9bbf3e5c258e9070351ea61cd11720f
HassanMunene <awanzihassan@gmail.com>
2024-05-29 15:51:36 +0300

    Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+class Rectangle that inherits from class Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    The rectangle class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        getter method for width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        setter method for width
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        getter method for height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        setter method for height
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        getter method for x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        setter method for x
+        """
+        self.__x = value
+    @property
+    def y(self):
+        """
+        getter method for y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        setter method for y
+        """
+        self.__y = value
