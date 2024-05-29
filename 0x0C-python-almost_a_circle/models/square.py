9fefade870f0b291c4fd5e815d7721733823edf8
HassanMunene <awanzihassan@gmail.com>
2024-05-29 18:29:18 +0300

    pycodesyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
-
     @property
     def size(self):
         """retrieve the value of width"""
