c943ac0eea88a50bce44ae73abfe627375f4166b
HassanMunene <awanzihassan@gmail.com>
2024-05-30 13:01:58 +0300

    save a json representation of instances to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -29,3 +29,16 @@ class Base:
             return "[]"
         json_rep = json.dumps(list_dictionaries)
         return json_rep
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        write json string representation to a file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
