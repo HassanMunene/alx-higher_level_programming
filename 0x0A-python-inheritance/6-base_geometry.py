ff14f30c76a038eba56c9b0402b4b61cc0f8a20b
HassanMunene <awanzihassan@gmail.com>
2024-06-01 14:35:35 +0300

    raising exceptions with messages

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+declare a class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGometry class here
+    """
+    def area(self):
+        """
+        raise an exception when called
+        """
+        raise Exception("area() is not implemented")
