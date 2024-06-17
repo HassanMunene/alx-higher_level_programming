02b0e05a726fc9c4e9bc1549119018cc3eaf9a47
HassanMunene <awanzihassan@gmail.com>
2024-06-17 17:20:57 +0300

    c is cool

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv;
+if (args[2] === undefined) {
+  console.log('undefined is undefined');
+} else if (args[3] === undefined) {
+  console.log(`${args[2]} is undefined`);
+} else {
+  console.log(`${args[2]} is ${args[3]}`);
+}
