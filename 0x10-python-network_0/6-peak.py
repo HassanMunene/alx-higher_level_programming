b7ac64374b31ffe6337d68d8e3d465a46e80056f
HassanMunene <awanzihassan@gmail.com>
2024-07-27 14:26:36 +0300

    technical interview

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+"""
+This module defines binary search
+"""
 def find_peak(list_of_integers):
+    """
+    The function to define the serach
+    """
     def binary_search_peak(start, end):
+        """
+        binary function
+        """
         if start == end:
             return list_of_integers[start]
 
         mid = (start + end) // 2
 
         # Check if mid element is a peak
-        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1]) and \
-           (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])\
+                and (mid == len(list_of_integers) - 1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
             return list_of_integers[mid]
 
         # If the left neighbor is greater, then a peak must be on the left side
