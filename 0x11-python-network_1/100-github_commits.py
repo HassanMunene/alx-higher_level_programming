4925d368d7b58f02877bbdc37ce64b8f0c86e6b8
HassanMunene <awanzihassan@gmail.com>
2024-07-27 20:25:49 +0300

    technical interview challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,17 +7,22 @@ using use the GitHub API. and Print
 import requests
 import sys
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     repo = sys.argv[1]
     owner = sys.argv[2]
-    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
-    response = requests.get(url)
-    json_resp = response.json()
 
-for commit in json_resp[:10]:
-    commit_data = commit['commit']
-    sha = commit_data.get('sha')
-    author = commit_data.get('author').get('name')
-    print(f"{sha}: {author}")
-    pass
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    params = {
+        'per_page': 10
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code != 200:
+        print("Error: Unable to fetch commits")
+
+    commits = response.json()
+
+    for commit in commits:
+        sha = commit.get('sha')
+        author_name = commit.get('commit', {}).get('author', {}).get('name')
+        print(f"{sha}: {author_name}")
