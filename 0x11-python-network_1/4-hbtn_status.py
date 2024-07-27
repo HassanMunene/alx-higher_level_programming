beb5810978f4f7cc3b61c9dc5e4308796e5eafbb
HassanMunene <awanzihassan@gmail.com>
2024-07-27 14:51:25 +0300

    using request package to make http requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+fetchiing https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == "__main__":
+
+    response = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
+
+    pass
